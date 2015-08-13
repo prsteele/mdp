b9b49545a4bbc9ff878b05141a74724f3f9dd5e2
Patrick Steele <prs233@cornell.edu>
2015-08-13 12:41:58 -0400

    Testing seems to be working.
    I had to add an ugly Show declaration for MDP, I'm hoping to refactor it
    to the testing module.

--- testsuite/tests/MDP/MDPTest.hs
+++ testsuite/tests/MDP/MDPTest.hs
@@ -1,6 +1,9 @@
 {-# Language FlexibleInstances #-}
 
+import Debug.Trace
+
 import Algorithms.MDP.MDP as Q
+import Algorithms.MDP.ValueIteration
 
 import Control.Monad
 import qualified Data.Map as Map
@@ -35,27 +38,39 @@ arbitraryActionSet states actions = do
 -- vector.
 arbitraryPDF :: [a] -> Gen [Double]
 arbitraryPDF xs = do
-  probs <- (vector (length xs)) `suchThat` (all (>= 0)) :: Gen [Double]
-  return [p / (sum probs) | p <- probs]
+  probs <- (vector (length xs)) :: Gen [Double]
+  return [abs p / (sum (map abs probs)) | p <- probs]
 
 arbitraryTransitionMatrix :: (Ord a, Ord b) =>
-                             [a] -> (a -> [b]) -> Gen (a -> a -> b -> Double)
+                             [a] -> (a -> [b]) -> Gen (b -> a -> a -> Double)
 arbitraryTransitionMatrix states actionSet = let
   pairs = [(s, a) | s <- states, a <- actionSet s]
   count = length pairs
   pdfToMap pdf = Map.fromList (zip states pdf)
   masterMap pdfs = Map.fromList (zip pairs (map pdfToMap pdfs))
+
+  trans master a s t = let
+    p = Map.lookup (s, a) master >>= Map.lookup t
+    in (case p of
+          Just z -> z
+          Nothing -> 0)
   in do
     pdfs <- replicateM count (arbitraryPDF states)
-    return $ (\s t a -> ((masterMap pdfs) Map.! (s, a)) Map.! t)
+    return (trans (masterMap pdfs))
 
 arbitraryCostFunction :: (Ord a, Ord b) =>
-                         [a] -> (a -> [b]) -> Gen (a -> b -> Double)
+                         [a] -> (a -> [b]) -> Gen (b -> a -> Double)
 arbitraryCostFunction states actionSet = let
   pairs = [(s, a) | s <- states, a <- actionSet s]
+
+  costFn costMap a s = case Map.lookup (s, a) costMap of
+    Just z -> z
+    Nothing -> 0
+
   in do
     costs <- vector (length pairs) `suchThat` (all (>= 0))
-    return $ curry ((Map.fromList (zip pairs costs)) Map.!)
+    --return $ curry ((Map.fromList (zip pairs costs)) Map.!)
+    return (costFn (Map.fromList (zip pairs costs)))
 
 arbitraryDiscount :: Gen Double
 arbitraryDiscount =
@@ -67,30 +82,36 @@ arbitraryDiscount =
 
 instance Arbitrary (Q.MDP Int Int) where
   arbitrary = do
-    states    <- arbitraryIntegerSequence 1 20
-    actions   <- arbitraryIntegerSequence 1 10
+    states    <- arbitraryIntegerSequence 1 10
+    actions   <- arbitraryIntegerSequence 1 3
     actionSet <- arbitraryActionSet states actions
-    costFn    <- arbitraryCostFunction actions actionSet
-    transMat  <- arbitraryTransitionMatrix actions actionSet
-    discount  <- arbitrary
+    costFn    <- arbitraryCostFunction states actionSet
+    transMat  <- arbitraryTransitionMatrix states actionSet
+    discount  <- arbitraryDiscount
     return $ mkMDP states actions transMat costFn actionSet discount
 
 prop_idempotent :: Int -> Bool
 prop_idempotent x = id x == id x
 
-increasing :: (Ord a) => [a] -> 
+increasing :: (Ord a) => [a] -> Bool
+increasing (p:q:qs) = if p <= q
+                      then increasing (q:qs)
+                      else False
+increasing _        = True
 
-prop_increasing :: (Q.MDP a b) -> Bool
+prop_increasing :: (Ord a, Ord b) => (Q.MDP a b) -> Bool
 prop_increasing mdp = let
   iterations = valueIteration mdp
+  valuesFor s = map snd (map ($ s) iterations)
 
-  increasingFor s = take 10 (map snd (map ($ s) iterations))
-  
-  values = map 
+  increasingFor s = increasing (take 10 (valuesFor s))
+  in all increasingFor (unStates mdp)
 
 -- -- prop_isStochastic :: ValidTrans -> Bool
 -- -- prop_isStochastic (ValidTrans trans) = let
 -- --   mdp = mkMDP states actions trans (\a b -> 0) (\a -> actions) 1.0
 -- --   in isStochastic mdp 0
 
-main = quickCheck prop_idempotent
+example = mkMDP [1] [1, 2] (\a _ _ -> if a == 2 then 1 else 0) (\_ _ -> 2.67) (\_ -> [2]) 0.5
+
+main = quickCheck (prop_increasing :: (Q.MDP Int Int) -> Bool)

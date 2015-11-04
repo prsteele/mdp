546979f6866ead168a1c2a6a45979eca2eed174a
Patrick Steele <prs233@cornell.edu>
2015-11-04 17:28:29 -0500

    Improved readability.
    Added a bit of documentation, and re-ordered the definitions to place
    the user-facing functions and types on top.

--- src/Algorithms/MDP/FlatMDP.hs
+++ src/Algorithms/MDP/FlatMDP.hs
@@ -5,13 +5,15 @@ import qualified Data.Vector as V
 
 import Algorithms.MDP.MDP (CostFunction)
 
--- | A state in the MDP.
-newtype State = State Int
-              deriving (Eq, Show)
+-- | A type representing an action- and state-dependent probablity
+-- vector.
+type Transitions a b t = b -> a -> a -> t
 
--- | An action in the MDP.
-newtype Action = Action Int
-              deriving (Eq, Show)
+-- | A type representing an action- and state-dependent cost.
+type Costs a b t = b -> a -> t
+
+-- | A type representing the allowed actions in a state.
+type ActionSet a b = a -> [b]
 
 -- | A cost function is a list of (State, Action, cost) triples
 -- describing the cost of being in the given state and the action
@@ -20,49 +22,65 @@ type FlatCostFunction a b t = V.Vector (a, b, t)
 
 type FlatCostFunctionBounds a b t = (FlatCostFunction a b t, t, t)
 
--- | An efficient representation of an MDP.
---
--- We pack all transition probabilities and costs into vectors.
-data FlatMDP a b t = FlatMDP
-                     { __states  :: V.Vector a
-                     , __actions :: V.Vector b
-                     , __states' :: V.Vector State
-                     , __actions' :: V.Vector Action
-                     , __costs   :: V.Vector (V.Vector t)
-                     , __trans   :: V.Vector (V.Vector (V.Vector t))
-                     , __discount :: t
-                     , __actionSet :: V.Vector (V.Vector Action)
-                     }
-
 mkFlatMDP :: (Eq b) =>
-             [a] -> [b] -> (b -> a -> a -> t) -> (b -> a -> t) -> (a -> [b]) -> t -> FlatMDP a b t
+             [a] -- ^ The state space
+          -> [b]  -- ^ The action space
+          -> Transitions a b t  -- ^ The transition probabilities
+          -> Costs a b t -- ^ The 
+          -> ActionSet a b
+          -> t
+          -> FlatMDP a b t
 mkFlatMDP states actions trans cost actionSet discount =
   let
-    __states = V.fromList states
-    __actions = V.fromList actions
-    __states' = V.fromList (map State [0..length states - 1])
-    __actions' = V.fromList (map Action [0..length actions - 1])
+    _states = V.fromList states
+    _actions = V.fromList actions
+    _states' = V.fromList (map State [0..length states - 1])
+    _actions' = V.fromList (map Action [0..length actions - 1])
     mkProbAS a s = V.fromList $ map (trans a s) states
     mkProbA a = V.fromList $ map (mkProbAS a) states
     mkCostA a = V.fromList $ map (cost a) states
 
-    __costs = V.fromList $ map mkCostA actions
-    __trans = V.fromList $ map mkProbA actions
+    _costs = V.fromList $ map mkCostA actions
+    _trans = V.fromList $ map mkProbA actions
 
     actionPairs = zip (map Action [0..]) actions
     actionSet' st = V.fromList $ map fst $ filter ((`elem` acs) . snd) actionPairs
       where
         acs = actionSet st
     
-    __actionSet = V.fromList $ map actionSet' states
+    _actionSet = V.fromList $ map actionSet' states
   in
     FlatMDP
-    { __states = __states
-    , __actions = __actions
-    , __states' = __states'
-    , __actions' = __actions'
-    , __costs = __costs
-    , __trans = __trans
-    , __discount = discount
-    , __actionSet = __actionSet
+    { _states = _states
+    , _actions = _actions
+    , _states' = _states'
+    , _actions' = _actions'
+    , _costs = _costs
+    , _trans = _trans
+    , _discount = discount
+    , _actionSet = _actionSet
     }
+
+-- | A state in the MDP.
+newtype State = State Int
+              deriving (Eq, Show)
+
+-- | An action in the MDP.
+newtype Action = Action Int
+              deriving (Eq, Show)
+
+-- | An efficient representation of an MDP.
+--
+-- We pack all transition probabilities and costs into vectors.
+data FlatMDP a b t = FlatMDP
+                     { _states  :: V.Vector a
+                     , _actions :: V.Vector b
+                     , _states' :: V.Vector State
+                     , _actions' :: V.Vector Action
+                     , _costs   :: V.Vector (V.Vector t)
+                     , _trans   :: V.Vector (V.Vector (V.Vector t))
+                     , _discount :: t
+                     , _actionSet :: V.Vector (V.Vector Action)
+                     }
+
+
--- src/Algorithms/MDP/FlatValueIteration.hs
+++ src/Algorithms/MDP/FlatValueIteration.hs
@@ -14,22 +14,22 @@ inner u v = V.sum (V.zipWith (*) u v)
 valueIteration :: (Ord t, Num t) => FlatMDP a b t -> [FlatCostFunction a b t]
 valueIteration mdp =
   let
-    states = __states mdp
-    actions = __actions mdp
+    states = _states mdp
+    actions = _actions mdp
     
     zero = V.replicate (V.length states) (V.head states, V.head actions, 0)
   in
     iterate (valueIterate mdp) zero
 
 valueIterate :: (Ord t, Num t) => FlatMDP a b t -> FlatCostFunction a b t -> FlatCostFunction a b t
-valueIterate mdp cf = V.zipWith (choiceFor mdp cf) (__states' mdp) (__states mdp)
+valueIterate mdp cf = V.zipWith (choiceFor mdp cf) (_states' mdp) (_states mdp)
 
 choiceFor :: (Ord t, Num t) => FlatMDP a b t -> FlatCostFunction a b t -> State -> a -> (a, b, t)
 choiceFor mdp cf (State st) s =
   let
     cmp (_, x) (_, y) = compare x y
-    costs = V.map (costForAction mdp cf (State st)) (__actionSet mdp V.! st)
-    pairs = V.zip (__actions mdp) costs
+    costs = V.map (costForAction mdp cf (State st)) (_actionSet mdp V.! st)
+    pairs = V.zip (_actions mdp) costs
     (ac, c) = V.minimumBy cmp pairs
   in
     (s, ac, c)
@@ -38,16 +38,16 @@ costForAction :: (Num t) =>
                  FlatMDP a b t -> FlatCostFunction a b t -> State -> Action -> t
 costForAction mdp cf (State st) (Action ac) =
   let
-    alpha = __discount mdp
-    fixedCost = (__costs mdp) V.! ac V.! st
-    transCost = inner (__trans mdp V.! ac V.! st) (V.map (\(_, _, c) -> c) cf)
+    alpha = _discount mdp
+    fixedCost = (_costs mdp) V.! ac V.! st
+    transCost = inner (_trans mdp V.! ac V.! st) (V.map (\(_, _, c) -> c) cf)
   in
     fixedCost + alpha * transCost
 
 relativeValueIteration mdp =
   let
-    states = __states mdp
-    actions = __actions mdp
+    states = _states mdp
+    actions = _actions mdp
     
     zero = V.replicate (V.length states) (V.head states, V.head actions, 0)
   in
@@ -59,7 +59,7 @@ relativeValueIterate :: (Ord t, Fractional t) =>
                         -> FlatCostFunctionBounds a b t
 relativeValueIterate mdp (cf, _, _) =
   let
-    alpha = __discount mdp
+    alpha = _discount mdp
     cf' = valueIterate mdp cf
     (lb, ub) = (V.minimum diffs, V.maximum diffs)
       where
@@ -67,3 +67,4 @@ relativeValueIterate mdp (cf, _, _) =
     scale = alpha / (1 - alpha)
   in 
     (cf', scale * lb, scale * ub)
+
